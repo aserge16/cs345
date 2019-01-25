6240e0ef693fdf6f8c13134274faabf94ab0d966
aserge16 <aleks.sergeev2@gmail.com>
2019-01-24 20:31:02 -0500

    Added timings to traversals

--- lab1/array-traversal.c
+++ lab1/array-traversal.c
@@ -5,28 +5,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+
 
 int main(int argc, char* argv[]) {
 
     int i, j, k;
+    float total_time;
+    struct timeval start, end;
     /* Get dimension size of array from argv */
     int dimension = atoi(argv[1]);
     /* Define array local to function */
     char myArray[dimension][dimension];
 
+    gettimeofday(&start, NULL);
     /* Row major traversal */
     for (i = 0; i < dimension; i++) {
         for (j = 0; j < dimension; j++) {
                 myArray[i][j] = 'r';
         }
     }
+    gettimeofday(&end, NULL);
 
+    /* Compute and print to stdout row traversal time */
+    total_time = end.tv_sec - start.tv_sec;
+    printf("Row traversal total time was %f seconds.\n", total_time);
+
+
+    gettimeofday(&start, NULL);
     /* Column major traversal */
     for (i = 0; i < dimension; i++) {
         for (j = 0; j < dimension; j++) {
                 myArray[j][i] = 'c';
         }
     }
+    gettimeofday(&end, NULL);
+
+    /* Compute and print to stdout column traversal time */
+    total_time = end.tv_sec - start.tv_sec;
+    printf("Column traversal total time was %f seconds.\n", total_time);
 
     return 0;
 }
