6ec7d1b6b4ee2de18e44a5946bc5866a55179531
aserge16 <aleks.sergeev2@gmail.com>
2019-01-28 21:14:20 -0500

    lab1 - added python list and numpy conversion times

--- lab1/python-traversal.py
+++ lab1/python-traversal.py
@@ -5,6 +5,8 @@
 # Aleksandr Sergeev
 
 import sys
+import timeit
+import numpy as np
 
 dimension = int(sys.argv[1])
 
@@ -12,11 +14,41 @@ def main():
     
     myArray = [['e' for i in range(dimension)] for j in range(dimension)]
 
-#   Row major traversal
+    start = timeit.default_timer()
+#   Row major traversal list
     for i in range(dimension):
         for j in range(dimension):
+            myArray[i][j] = 'r'
+    stop = timeit.default_timer()
+    print('List row major traversal time: ', stop - start)
 
 
-#   Column major traversal
+    start = timeit.default_timer()
+#   Column major traversal list
+    for i in range(dimension):
+        for j in range(dimension):
+            myArray[j][i] = 'c'
+    stop = timeit.default_timer()
+    print('List column major traversal time: ', stop - start)
+
+
+    ndArray = np.ones((dimension, dimension), dtype = np.int8)
+
+    start = timeit.default_timer()
+#   Row major traversal np
+    for i in range(dimension):
+        for j in range(dimension):
+            ndArray[i][j] = 2
+    stop = timeit.default_timer()
+    print('ndArray row major traversal time: ', stop - start)
+
+    start = timeit.default_timer()
+#   Column major traversal np
+    for i in range(dimension):
+        for j in range(dimension):
+            ndArray[j][i] = 3
+    stop = timeit.default_timer()
+    print('ndArray column major traversal time: ', stop - start)
+
 
 main()
