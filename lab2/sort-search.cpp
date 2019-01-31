510868efae29af9cb74f36cc9d025557ada4c3aa
aserge16 <aleks.sergeev2@gmail.com>
2019-01-31 17:04:32 -0500

    lab2 - Vector array initialized

--- lab2/sort-search.cpp
+++ lab2/sort-search.cpp
@@ -6,8 +6,32 @@ Quicksort Implimentation */
 
 #include <iostream>
 #include <fstream>
-#include <bits/stdc++.h>
 #include <vector>
 
+using namespace std;
+
 int main(int argc, char *argv[]) {
+    vector<int> vector_Array ;
+    int x, vec_size ;
+
+    /* Check for bash file input */
+    if (argc != 2) {
+        cout << "Incorrect bash input.\n" ;
+        exit(1) ;
+    }
+    else {
+        ifstream int_File(argv[1]) ;
+        /* Check if file can be opened */
+        if ( !int_File.is_open() ) {
+            cout << "Could not open int file.\n" ;
+        }
+        else {
+            while ( int_File >> x) {
+                vector_Array.push_back(x) ;
+            }
+            vec_size = vector_Array.size() ;
+            int_File.close() ;
+        }
+
+    }
 }
