aa0c44f602f89fd8d8d8ad85a74c093cc2ca4b1a
aserge16 <aleks.sergeev2@gmail.com>
2019-02-05 14:03:06 -0500

    lab2 - Progress on parition function

--- lab2/sort-search.cpp
+++ lab2/sort-search.cpp
@@ -7,18 +7,35 @@ Quicksort Implimentation */
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 
 /* Partition function implimentation */
-int partition(int array[], int low, int high) {
-    return 0 ;
+int partition(vector<int> array, int low, int high) {
+    /* Declare Pivot, at ending index of array */
+    int pivot = array[high];
+    /* Lowest element to begin comparison */
+    int i = low;
+
+    /* Loop through array and pivot swap */
+    for ( int j = low + 1; j < high; j++) {
+        // If lowest less than or equal to pivot, swap */
+        if (array[j] <= pivot) {
+            i++ ;
+            iter_swap(array.begin() + i, array.begin() + j) ;
+        }
+    }
+
+    /* Insert pivot into correct position, return index */
+    iter_swap(array.begin() + i + 1, array.begin() + high) ;
+    return (i + 1) ;
 }
 
 
 /* Quicksort function implimentation */
-void quicksort(int array[], int low, int high) {
+void quicksort(vector<int> array, int low, int high) {
     if (low < high) {
         /* Partition array */
         int x = partition(array, low, high) ;
@@ -27,6 +44,11 @@ void quicksort(int array[], int low, int high) {
         quicksort(array, low, x - 1) ;
         quicksort(array, x + 1, high) ;
     }
+    for (int x = 0; x < array.size(); x++) {
+        cout << array[x] ;
+        cout << "\n" ;
+    }
+    cout << "\n\n" ;
 }
 
 
@@ -52,7 +74,11 @@ int main(int argc, char *argv[]) {
             vec_size = vector_Array.size() ;
             int_File.close() ;
             /* Call quicksort */
-            quicksort(vector_Array, 1, vec_size)
+            quicksort(vector_Array, 0, vec_size - 1) ;
+            for (int x = 0; x < vec_size; x++) {
+                cout << vector_Array[x] ;
+                cout << "\n" ;
+            }
         }
     }
 }
