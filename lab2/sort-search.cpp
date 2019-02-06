a966c600a749b2a65b132f7fe2606daed8c3ed0f
aserge16 <aleks.sergeev2@gmail.com>
2019-02-05 23:39:34 -0500

    lab2 - Changed from vector to static array

--- lab2/sort-search.cpp
+++ lab2/sort-search.cpp
@@ -11,33 +11,41 @@ Quicksort Implimentation */
 
 using namespace std;
 
+/* Swap function */
+void swap(int* a, int* b)
+{
+    int t=*a;
+    *a=*b;
+    *b=t;
+}
 
 /* Partition function implimentation */
-int partition(vector<int> array, int low, int high) {
+int partition(int array[], int low, int high) {
     /* Declare Pivot, at ending index of array */
-    int pivot = array.at(high);
+    int pivot = array[high];
     /* Lowest element to begin comparison */
     int i = low - 1;
 
     /* Loop through array and pivot swap */
     for ( int j = low; j < high; j++) {
         // If lowest less than or equal to pivot, swap */
-        if (array.at(j) <= pivot) {
+        if (array[j] <= pivot) {
             i++ ;
             int temp = array[i] ;
             array[i] = array[j] ;
-            array[j] = array[i] ;
+            array[j] = temp ;
+            swap(&array[i], &array[j]) ;
         }
     }
 
     /* Insert pivot into correct position, return index */
-    swap(array[i+1], array[high]) ;
+    swap(&array[i+1], &array[high]) ;
     return (i + 1) ;
 }
 
 
 /* Quicksort function implimentation */
-void quicksort(vector<int> array, int low, int high) {
+void quicksort(int array[], int low, int high) {
     if (low < high) {
         /* Partition array */
         int x = partition(array, low, high) ;
@@ -50,9 +58,6 @@ void quicksort(vector<int> array, int low, int high) {
 
 
 int main(int argc, char *argv[]) {
-    vector<int> vector_Array ;
-    int x, vec_size ;
-
     /* Check for bash file input */
     if (argc != 2) {
         cout << "Incorrect bash input. Please run the code followed by the name of file.\n" ;
@@ -65,17 +70,22 @@ int main(int argc, char *argv[]) {
             cout << "Could not open int file.\n" ;
         }
         else {
-            while ( int_File >> x) {
-                vector_Array.push_back(x) ;
+            /* Count number of lines in file for static array decleration */
+            int size = count(istreambuf_iterator<char>(int_File), istreambuf_iterator<char>(), '\n');
+            int x, y = 0;
+            string line ;
+            int array[size] ;
+
+            while (! int_File.eof()) {
+                y++ ;
             }
-            vec_size = vector_Array.size() ;
-            int_File.close() ;
-            /* Call quicksort */
-            quicksort(vector_Array, 0, vec_size - 1) ;
-            for (int x = 0; x < 29; x++) {
-                cout << vector_Array.at(x) ;
+            for (x = 0; x < 29; x++) {
+                cout << array[x] ;
                 cout << "\n" ;
             }
+            int_File.close() ;
+            /* Call quicksort */
+            quicksort(array, 0, size - 1) ;
         }
     }
 }
